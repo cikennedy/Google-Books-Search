09a85807bcc21f9c8386037ebc74d2b1595bb3f8
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-14 16:43:31 -0500

    defined all methods in bookController

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,5 +1,29 @@
 const db = require("../models");
 
 module.exports = {
-    
-}
+    findAll: function(req, res) {
+        db.Book.find(req.query)
+        .then(dbData => res.json(dbData))
+        .catch(err => res.status(500).json(err));
+    },
+    findById: function(req, res) {
+        db.Book.findById(req.params.id)
+        .then(dbData => res.json(dbData))
+        .catch(err => res.status(500).json(err));
+    },
+    create: function(req, res) {
+        db.Book.create(req.body)
+        .then(dbData => res.json(dbData))
+        .catch(err => res.status(500).json(err));
+    },
+    update: function(req, res) {
+        db.Book.findOneAndUpdate({ id: req.params.id }, req.body) 
+        .then(dbData => res.json(dbData))
+        .catch(err => res.status(500).json(err));
+    },
+    remove: function(req, res) {
+        db.Book.findById(req.params.id)
+        .then(dbData => res.json(dbData))
+        .catch(err => res.status(500).json(err));
+    },
+};
