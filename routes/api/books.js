40353836bb2334ee96eb2bf4fd58bf2d7b7ef4b4
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-14 16:56:23 -0500

    added all necessary routes to google and books api files

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,5 +1,14 @@
 const router = require("express").Router();
+const bookController = require("../../controllers/bookController");
 
 // add api routes below 
+router.route("/")
+    .get(bookController.findAll)
+    .post(bookController.create);
+
+router.route("/:id")
+    .get(bookController.findById)
+    .delete(bookController.remove)
+    .put(bookController.update);
 
 module.exports = router;
--- routes/api/google.js
+++ routes/api/google.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
+const googleController = require("../../controllers/googleController");
 
 // add api routes below 
+router.route("/").get(googleController.findAll);
 
 module.exports = router;
