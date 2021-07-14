964f26de837f05c988ce6df9291530410ec76e3a
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-14 15:05:45 -0500

    added book model with necessary fields and datatypes

--- models/book.js
+++ models/book.js
@@ -2,13 +2,31 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    // title:
-    // authors:
-    // description:
-    // image:
-    // link:
-    // googleId:
-
+    title: {
+        type: String,
+        required: true
+    },
+    authors: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    googleId: {
+        type: String,
+        required: true,
+        unique: true
+    }
 });
 
 const Book = mongoose.model("Book", bookSchema);
