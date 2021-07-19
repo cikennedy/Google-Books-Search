52ec5cac75fd0ab3a73eef919fd42e774a30b3ff
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-19 15:59:05 -0500

    added navbar with links to search and saved books

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Saved from "./pages/Saved";
 import Search from "./pages/Search";
-// import Nav from "./components/Nav";
+import Nav from "./Components/Nav";
 
 function App() {
   return (
     <Router>
       <div>
-        {/* <Nav /> */}
+        <Nav />
         <Switch>
           <Route exact path="/" component={Search} />
           {/* <Route exact path="/saved" component={Saved} /> */}
