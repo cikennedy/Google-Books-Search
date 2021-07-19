ff2e6f0d48cef96d0fbcdd79394692f64479a564
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-19 10:13:55 -0500

    changes to search and save functions

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,24 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 // import Link from '@material-ui/core/Link';
 // import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
+// import Box from '@material-ui/core/Box';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-
-// class Search extends Component {
-//     state = {
-//         books: [],
-//         q: "",
-//         message: "Search for a book..."
-//     };
+import API from "../utils/API";
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -41,8 +34,14 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function Search() {
-    const [formObject, setFormObject] = useState({})
+    const [formObject, setFormObject] = useState({});
+    // const [books, setBooks] = useState({res.data});
     const classes = useStyles();
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+      getSearchedBooks();
+    }, []);
   
     function handleInputChange(event) {
       const { name, value } = event.target;
@@ -51,8 +50,52 @@ function Search() {
   
     async function handleFormSubmit(event) {
       event.preventDefault();
-      //getBooks();
+      getSearchedBooks();
     };
+
+    const getSearchedBooks = () => {
+      API.getBooks(q).then((res) => {
+        const allBookData = res.data;
+        console.log('hello');
+        console.log(allBookData);
+        setBooks(() => allBookData);
+      })
+      .catch(err => console.log(err));
+    };
+
+    // add message if no books are found 
+    // const {id} = useParams()
+    useEffect(() => {
+      API.saveBook({
+        googleId: book.id,
+        title: book.volumeInfo.title,
+        authors: book.volumeInfo.authors,
+        description: book.volumeInfo.description,
+        image: book.volumeInfo.image,
+        link: book.volumeInfo.link
+      }).then((res) => getSearchedBooks())
+    })
+
+    // const saveSearchedBook = (id) => {
+    //   const book = 
+    // }
+    
+    
+      handleBookSave = id => {
+        const book = this.state.books.find(book => book.id === id);
+    
+        API.saveBook({
+          googleId: book.id,
+          title: book.volumeInfo.title,
+          subtitle: book.volumeInfo.subtitle,
+          link: book.volumeInfo.infoLink,
+          authors: book.volumeInfo.authors,
+          description: book.volumeInfo.description,
+          image: book.volumeInfo.imageLinks.thumbnail
+        }).then(() => this.getBooks());
+      };
+
+    // BREAK // 
     
     return (
         <Container component="main" maxWidth="xs">
@@ -88,8 +131,37 @@ function Search() {
             </Button>
             </form>
         </div>
-        <Box mt={8}>
-        </Box>
+        {/* <Row>
+          <Col size="md-12">
+            <Card title="Results">
+              {this.state.books.length ? (
+                <List>
+                  {this.state.books.map(book => (
+                    <Book
+                      key={book.id}
+                      title={book.volumeInfo.title}
+                      subtitle={book.volumeInfo.subtitle}
+                      link={book.volumeInfo.infoLink}
+                      authors={book.volumeInfo.authors.join(", ")}
+                      description={book.volumeInfo.description}
+                      image={book.volumeInfo.imageLinks.thumbnail}
+                      Button={() => (
+                        <button
+                          onClick={() => this.handleBookSave(book.id)}
+                          className="btn btn-primary ml-2"
+                        >
+                          Save
+                        </button>
+                      )}
+                    />
+                  ))}
+                </List>
+              ) : (
+                <h2 className="text-center">{this.state.message}</h2>
+              )}
+            </Card>
+          </Col>
+        </Row> */}
         </Container>
     );
   }
