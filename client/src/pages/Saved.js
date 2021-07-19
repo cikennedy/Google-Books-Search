1b675c494f61cc143591e1ebc67eeda3dc6548f2
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-19 16:10:57 -0500

    added features to saved page

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,147 +1,108 @@
-// import React, { Component } from "react";
-// import Avatar from '@material-ui/core/Avatar';
-// import Button from '@material-ui/core/Button';
-// import CssBaseline from '@material-ui/core/CssBaseline';
-// import TextField from '@material-ui/core/TextField';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import Checkbox from '@material-ui/core/Checkbox';
+import React, { Component } from "react";
+// import { useParams } from "react-router-dom";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
 // import Link from '@material-ui/core/Link';
-// import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 // import Box from '@material-ui/core/Box';
-// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-// import Typography from '@material-ui/core/Typography';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Container from '@material-ui/core/Container';
-// import API from "../utils/API";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+// import Container from "@material-ui/core/Container";
+import { Container } from "../Components/Container";
+import Card from "../Components/Card";
+// import List from "@material-ui/core/List";
+// import ListItem from "../Components/ListItem";
+import { List } from "../Components/List";
+import GridList from '@material-ui/core/GridList';
+import FeaturedPost from "../Components/FeaturedPost";
+// import Form from '../Components/Form';
+import API from "../utils/API";
 
-// class Saved extends Component {
-//     state = {
-//         books: [],
-//     };
+const styles = (theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: "100%", // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+    root: {
+      width: "100%",
+      maxWidth: 360,
+      backgroundColor: theme.palette.background.paper,
+    },
+  });
 
-//     useStyles = makeStyles((theme) => ({
-//         icon: {
-//           marginRight: theme.spacing(2),
-//         },
-//         heroContent: {
-//           backgroundColor: theme.palette.background.paper,
-//           padding: theme.spacing(8, 0, 6),
-//         },
-//         heroButtons: {
-//           marginTop: theme.spacing(4),
-//         },
-//         cardGrid: {
-//           paddingTop: theme.spacing(8),
-//           paddingBottom: theme.spacing(8),
-//         },
-//         card: {
-//           height: '100%',
-//           display: 'flex',
-//           flexDirection: 'column',
-//         },
-//         cardMedia: {
-//           paddingTop: '56.25%', // 16:9
-//         },
-//         cardContent: {
-//           flexGrow: 1,
-//         },
-//         footer: {
-//           backgroundColor: theme.palette.background.paper,
-//           padding: theme.spacing(6),
-//         },
-//       }));
+class Saved extends Component {
+  state = {
+    books: []
+  };
 
-//     // const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-//     // const classes = useStyles();
-//     render() {
-//         const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-//         const classes = useStyles();
-//         return (
-//             <React.Fragment>
-//               <CssBaseline />
-//               <AppBar position="relative">
-//                 <Toolbar>
-//                   <CameraIcon className={classes.icon} />
-//                   <Typography variant="h6" color="inherit" noWrap>
-//                     Album layout
-//                   </Typography>
-//                 </Toolbar>
-//               </AppBar>
-//               <main>
-//                 {/* Hero unit */}
-//                 <div className={classes.heroContent}>
-//                   <Container maxWidth="sm">
-//                     <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-//                       Album layout
-//                     </Typography>
-//                     <Typography variant="h5" align="center" color="textSecondary" paragraph>
-//                       Something short and leading about the collection below—its contents, the creator, etc.
-//                       Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-//                       entirely.
-//                     </Typography>
-//                     <div className={classes.heroButtons}>
-//                       <Grid container spacing={2} justifyContent="center">
-//                         <Grid item>
-//                           <Button variant="contained" color="primary">
-//                             Main call to action
-//                           </Button>
-//                         </Grid>
-//                         <Grid item>
-//                           <Button variant="outlined" color="primary">
-//                             Secondary action
-//                           </Button>
-//                         </Grid>
-//                       </Grid>
-//                     </div>
-//                   </Container>
-//                 </div>
-//                 <Container className={classes.cardGrid} maxWidth="md">
-//                   {/* End hero unit */}
-//                   <Grid container spacing={4}>
-//                     {cards.map((card) => (
-//                       <Grid item key={card} xs={12} sm={6} md={4}>
-//                         <Card className={classes.card}>
-//                           <CardMedia
-//                             className={classes.cardMedia}
-//                             image="https://source.unsplash.com/random"
-//                             title="Image title"
-//                           />
-//                           <CardContent className={classes.cardContent}>
-//                             <Typography gutterBottom variant="h5" component="h2">
-//                               Heading
-//                             </Typography>
-//                             <Typography>
-//                               This is a media card. You can use this section to describe the content.
-//                             </Typography>
-//                           </CardContent>
-//                           <CardActions>
-//                             <Button size="small" color="primary">
-//                               View
-//                             </Button>
-//                             <Button size="small" color="primary">
-//                               Edit
-//                             </Button>
-//                           </CardActions>
-//                         </Card>
-//                       </Grid>
-//                     ))}
-//                   </Grid>
-//                 </Container>
-//               </main>
-//               {/* Footer */}
-//               <footer className={classes.footer}>
-//                 <Typography variant="h6" align="center" gutterBottom>
-//                   Footer
-//                 </Typography>
-//                 <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-//                   Something here to give the footer a purpose!
-//                 </Typography>
-//                 <Copyright />
-//               </footer>
-//               {/* End footer */}
-//             </React.Fragment>
-//         );
-//     };
-// }
+  componentDidMount() {
+    this.getSavedBooks();
+  }
 
-// export default Saved;
+  getSavedBooks = () => {
+    API.getSavedBooks()
+      .then(res =>
+        this.setState({
+          books: res.data
+        })
+      )
+      .catch(err => console.log(err));
+  };
+
+  handleBookDelete = id => {
+    API.deleteBook(id).then(res => this.getSavedBooks());
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Container>
+        <Card title="Saved Books">
+          {this.state.books.length ? (
+            <List>
+              {this.state.books.map((book) => (
+                <FeaturedPost
+                  key={book.id}
+                  title={book.volumeInfo.title}
+                  subtitle={book.volumeInfo.subtitle}
+                  link={book.volumeInfo.infoLink}
+                  authors={book.volumeInfo.authors.join(", ")}
+                  description={book.volumeInfo.description}
+                  image={book.volumeInfo.imageLinks.thumbnail}
+                  Button={() => (
+                    <button
+                      onClick={() => this.handleSaveBook(book.id)}
+                      className="btn btn-primary ml-2"
+                    >
+                      Delete from Saved Books
+                    </button>
+                  )}
+                />
+              ))}
+            </List>
+          ) : (
+            <h2 className="text-center">No Books Saved</h2>
+          )}
+        </Card>
+      </Container>
+    );
+  }
+}
+
+export default withStyles(styles)(Saved);
