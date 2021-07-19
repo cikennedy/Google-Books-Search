a9499845c12fc76c9981751c38c8e5f02a20eac9
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-07-19 15:26:39 -0500

    updated styling and functionality for search page

--- client/src/Components/FeaturedPost/index.js
+++ client/src/Components/FeaturedPost/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
+// import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Hidden from '@material-ui/core/Hidden';
+import { ListItem } from "../List";
+
 
 const useStyles = makeStyles({
   card: {
@@ -21,11 +23,11 @@ const useStyles = makeStyles({
   },
 });
 
-export default function FeaturedPost({ title, authors, link, description, image, button }) {
+export default function FeaturedPost({ title, authors, link, description, image, Button }) {
   const classes = useStyles();
 
   return (
-    <Grid item xs={12} md={6}>
+    <ListItem>
       <CardActionArea component="a" href="#">
         <Card className={classes.card}>
           <div className={classes.cardDetails}>
@@ -52,7 +54,7 @@ export default function FeaturedPost({ title, authors, link, description, image,
           </Hidden>
         </Card>
       </CardActionArea>
-    </Grid>
+    </ListItem>
   );
 }
 
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,38 +1,48 @@
 import React, { Component } from "react";
 // import { useParams } from "react-router-dom";
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
 // import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 // import Box from '@material-ui/core/Box';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Card from '../Components/Card';
-import FeaturedPost from '../Components/FeaturedPost';
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+// import Container from "@material-ui/core/Container";
+import { Container } from "../Components/Container";
+import Card from "../Components/Card";
+// import List from "@material-ui/core/List";
+// import ListItem from "../Components/ListItem";
+import { List } from "../Components/List";
+import GridList from '@material-ui/core/GridList';
+import FeaturedPost from "../Components/FeaturedPost";
 import API from "../utils/API";
 
 const styles = (theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
 });
 
 class Search extends Component {
@@ -42,32 +52,34 @@ class Search extends Component {
     message: "",
   };
 
-  handleInputChange = event => {
+  handleInputChange = (event) => {
     const { name, value } = event.target;
     this.setState({
-      [name]: value
+      [name]: value,
     });
   };
 
   getBooks = () => {
     API.getBooks(this.state.search)
-    .then((res) => this.setState({
-      books: res.data
-    })
-    ).catch(() => 
-    this.setState({
-      books: [],
-      message: "No books found with the given search."
-    })
-    );
+      .then((res) =>
+        this.setState({
+          books: res.data,
+        })
+      )
+      .catch(() =>
+        this.setState({
+          books: [],
+          message: "No books found with the given search.",
+        })
+      );
   };
 
-  handleFormSubmit = event => {
+  handleFormSubmit = (event) => {
     event.preventDefault();
     this.getBooks();
   };
 
-  handleSaveBook = id => {
+  handleSaveBook = (id) => {
     const book = this.state.books.find((book) => book.id === id);
 
     API.saveBook({
@@ -76,221 +88,153 @@ class Search extends Component {
       authors: book.volumeInfo.authors,
       description: book.volumeInfo.description,
       image: book.volumeInfo.image,
-      link: book.volumeInfo.link
+      link: book.volumeInfo.link,
     }).then(() => this.getBooks());
   };
 
   render() {
-  const { classes } = this.props;
+    const { classes } = this.props;
     return (
-    <Container component="main" maxWidth="xs">
-    <CssBaseline />
-    <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-        <MenuBookIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-       Search for a book!
-        </Typography>
-        <form className={classes.form} noValidate
-        handleFormSubmit={this.handleFormSubmit}
-        handleInputChange={this.handleInputChange}
-        search={this.state.search}
-        >
-        <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="search"
-            label="Search..."
-            name="search"
-            // autoComplete="Book"
-            autoFocus
-            onChange={this.handleInputChange}
-        />
-        <Button
-            onClick={this.handleFormSubmit}
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-        >
-            Search
-        </Button>
-        </form>
-    </div>
-
-    
-
-    <Container maxWidth="lg">
-    <Card title="Search Results">
-      {this.state.books.length ? (
-          <Grid container spacing={3}>
-            {this.state.books.map((book) => (
-              <FeaturedPost 
-              key={book.id} 
-              title={book.volumeInfo.title}
-              authors={book.volumeInfo.authors}
-              description={book.volumeInfo.description}
-              image={book.volumeInfo.imageLinks.thumbnail}
-              link={book.volumeInfo.infoLink}
-              Button={() => (
-                <Button
-                onClick={() => this.handleSaveBook(book.id)}
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}>
-                  Save Book
-                </Button>
-              )}
-             />
-             ))}
-          </Grid>
+      <Container>
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <MenuBookIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Search for a book!
+          </Typography>
+          <form
+            className={classes.form}
+            noValidate
+            handleFormSubmit={this.handleFormSubmit}
+            handleInputChange={this.handleInputChange}
+            search={this.state.search}
+          >
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="search"
+              label="Search for a book..."
+              name="search"
+              // autoComplete="Book"
+              autoFocus
+              onChange={this.handleInputChange}
+            />
+            <Button
+              onClick={this.handleFormSubmit}
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Search
+            </Button>
+          </form>
+        </div>
+        <Card title="Results">
+          {this.state.books.length ? (
+            <List>
+              {this.state.books.map((book) => (
+                <FeaturedPost
+                  key={book.id}
+                  title={book.volumeInfo.title}
+                  subtitle={book.volumeInfo.subtitle}
+                  link={book.volumeInfo.infoLink}
+                  authors={book.volumeInfo.authors.join(", ")}
+                  description={book.volumeInfo.description}
+                  image={book.volumeInfo.imageLinks.thumbnail}
+                  Button={() => (
+                    <button
+                      onClick={() => this.handleBookSave(book.id)}
+                      className="btn btn-primary ml-2"
+                    >
+                      Save
+                    </button>
+                  )}
+                />
+              ))}
+            </List>
           ) : (
-            <h2>{this.state.message}</h2>
+            <h2 className="text-center">{this.state.message}</h2>
           )}
         </Card>
       </Container>
-      
-    </Container>
     );
   }
 }
+
 export default withStyles(styles)(Search);
 
-// function Search() {
-//     const [formObject, setFormObject] = useState({});
-//     // const [books, setBooks] = useState({res.data});
-//     const classes = useStyles();
-//     const [books, setBooks] = useState([]);
-//     const [q, setQ] = useState("");
+// <Container component="main">
+// <CssBaseline />
+// <div className={classes.paper}>
+//     <Avatar className={classes.avatar}>
+//     <MenuBookIcon />
+//     </Avatar>
+//     <Typography component="h1" variant="h5">
+//    Search for a book!
+//     </Typography>
+//     <form className={classes.form} noValidate
+//     handleFormSubmit={this.handleFormSubmit}
+//     handleInputChange={this.handleInputChange}
+//     search={this.state.search}
+//     >
+//     <TextField
+//         variant="outlined"
+//         margin="normal"
+//         required
+//         fullWidth
+//         id="search"
+//         label="Search..."
+//         name="search"
+//         // autoComplete="Book"
+//         autoFocus
+//         onChange={this.handleInputChange}
+//     />
+//     <Button
+//         onClick={this.handleFormSubmit}
+//         fullWidth
+//         variant="contained"
+//         color="primary"
+//         className={classes.submit}
+//     >
+//         Search
+//     </Button>
+//     </form>
+// </div>
 
-//     useEffect(() => {
-//       getSearchedBooks();
-//     }, []);
-  
-//     function handleInputChange(event) {
-//       const { name, value } = event.target;
-//       setFormObject({...formObject, [name]: value})
-//     };
-  
-//     async function handleFormSubmit(event) {
-//       event.preventDefault();
-//       getSearchedBooks();
-//     };
-
-//     const getSearchedBooks = () => {
-//       API.getBooks(q).then((res) => {
-//         books = res.data;
-//         console.log('hello');
-//         console.log(books);
-//         setBooks(() => books);
-//       })
-//       .catch(err => console.log(err));
-//     };
-
-    // // add message if no books are found 
-    // // const {id} = useParams()
-    // useEffect(() => {
-    //   API.saveBook({
-    //     googleId: book.id,
-    //     title: book.volumeInfo.title,
-    //     authors: book.volumeInfo.authors,
-    //     description: book.volumeInfo.description,
-    //     image: book.volumeInfo.image,
-    //     link: book.volumeInfo.link
-    //   }).then((res) => getSearchedBooks())
-    // })
-
-    // const saveSearchedBook = (id) => {
-    //   const book = 
-    // }
-    
-    
-      // handleBookSave = id => {
-      //   const book = this.state.books.find(book => book.id === id);
-    
-      //   API.saveBook({
-      //     googleId: book.id,
-      //     title: book.volumeInfo.title,
-      //     subtitle: book.volumeInfo.subtitle,
-      //     link: book.volumeInfo.infoLink,
-      //     authors: book.volumeInfo.authors,
-      //     description: book.volumeInfo.description,
-      //     image: book.volumeInfo.imageLinks.thumbnail
-      //   }).then(() => this.getBooks());
-      // };
-
-    // BREAK // 
-    
-  //   return (
-  //       <Container component="main" maxWidth="xs">
-  //       <CssBaseline />
-  //       <div className={classes.paper}>
-  //           <Avatar className={classes.avatar}>
-  //           <MenuBookIcon />
-  //           </Avatar>
-  //           <Typography component="h1" variant="h5">
-  //          Search for a book!
-  //           </Typography>
-  //           <form className={classes.form} noValidate>
-  //           <TextField
-  //               variant="outlined"
-  //               margin="normal"
-  //               required
-  //               fullWidth
-  //               id="search"
-  //               label="Search..."
-  //               name="search"
-  //               // autoComplete="Book"
-  //               autoFocus
-  //               onChange={handleInputChange}
-  //           />
-  //           <Button
-  //               onClick={handleFormSubmit}
-  //               fullWidth
-  //               variant="contained"
-  //               color="primary"
-  //               className={classes.submit}
-  //           >
-  //               Search
-  //           </Button>
-  //           </form>
-  //       </div>
-  //       {/* <Row>
-  //         <Col size="md-12">
-  //           <Card title="Results">
-  //             {this.state.books.length ? (
-  //               <List>
-  //                 {this.state.books.map(book => (
-  //                   <Book
-  //                     key={book.id}
-  //                     title={book.volumeInfo.title}
-  //                     subtitle={book.volumeInfo.subtitle}
-  //                     link={book.volumeInfo.infoLink}
-  //                     authors={book.volumeInfo.authors.join(", ")}
-  //                     description={book.volumeInfo.description}
-  //                     image={book.volumeInfo.imageLinks.thumbnail}
-  //                     Button={() => (
-  //                       <button
-  //                         onClick={() => this.handleBookSave(book.id)}
-  //                         className="btn btn-primary ml-2"
-  //                       >
-  //                         Save
-  //                       </button>
-  //                     )}
-  //                   />
-  //                 ))}
-  //               </List>
-  //             ) : (
-  //               <h2 className="text-center">{this.state.message}</h2>
-  //             )}
-  //           </Card>
-  //         </Col>
-  //       </Row> */}
-  //       </Container>
-  //   );
-  // }
-  
+// <div className={classes.root}>
+//   <Card>
+// {this.state.books.length ? (
+//       <List className={classes.root}
+//       title="Search Results"
+//       component="nav" >
+//         {this.state.books.map((book) => (
+//           <FeaturedPost
+//           key={book.id}
+//           title={book.volumeInfo.title}
+//           authors={book.volumeInfo.authors}
+//           description={book.volumeInfo.description}
+//           image={book.volumeInfo.imageLinks.thumbnail}
+//           link={book.volumeInfo.infoLink}
+//           Button={() => (
+//             <Button
+//             onClick={() => this.handleSaveBook(book.id)}
+//             fullWidth
+//             variant="contained"
+//             color="primary"
+//             className={classes.submit}>
+//               Save Book
+//             </Button>
+//           )}
+//          />
+//          ))}
+//       </List>
+//       ) : (
+//         <h2>{this.state.message}</h2>
+//       )}
+//       </Card>
+//       </Container>
